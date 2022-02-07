b9eb2a211a14a1d51660844b8a1e931c843ae5de
Илья Пермиловский <lannyshark@ya.ru>
2022-02-07 23:49:04 +0300

    Добавьте файлы проекта.

---
+++ 14 WpfDataTemplate/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_WpfDataTemplate
+{
+    public enum ProductTypes
+    {
+        Food,
+        Tech
+    }
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+        public string ImageSource { get; set; }
+        public ProductTypes ProductType { get; set; }
+    }
+}
